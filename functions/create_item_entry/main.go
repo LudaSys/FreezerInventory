714f772e541f08506e931fbc4b2b79620978e49a
John Slowik <john@jslowik.com>
2022-08-30 18:29:43 -0500

    Simplified

--- functions/create_item_entry/main.go
+++ functions/create_item_entry/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"functions/shared/models"
 	"github.com/aws/aws-lambda-go/events"
 	runtime "github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -10,35 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	_ "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/google/uuid"
-	"log"
-	"strconv"
 )
-
-type TableBasics struct {
-	DynamoDbClient *dynamodb.Client
-	TableName      string
-}
-
-type FoodItem struct {
-	itemId string `json:"itemId"`
-	Name   string `json:"name"`
-	Time   string `json:"time"`
-}
-
-func (basics TableBasics) AddFoodItem(foodItem FoodItem) (bool, error) {
-	_, err := basics.DynamoDbClient.PutItem(context.TODO(), &dynamodb.PutItemInput{
-		TableName: aws.String("FoodItems"),
-		Item: map[string]types.AttributeValue{
-			"itemId": &types.AttributeValueMemberS{Value: foodItem.itemId},
-			"name":   &types.AttributeValueMemberS{Value: foodItem.Name},
-			"time":   &types.AttributeValueMemberS{Value: "12345"},
-		},
-	})
-	if err != nil {
-		log.Printf("Couldn't add item to table. Here's why: %v\n", err)
-	}
-	return false, err
-}
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), func(o *config.LoadOptions) error {
@@ -48,18 +21,27 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	if err != nil {
 		panic(err)
 	}
-
-	result, err := TableBasics.AddFoodItem(TableBasics{
-		DynamoDbClient: dynamodb.NewFromConfig(cfg),
-		TableName:      "FoodItems",
-	}, FoodItem{
-		itemId: uuid.New().String(),
+	foodItem := models.FoodItem{
+		ItemId: uuid.New().String(),
 		Name:   "Banana",
+	}
+
+	svc := dynamodb.NewFromConfig(cfg)
+	_, err = svc.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String("FoodItems"),
+		Item: map[string]types.AttributeValue{
+			"itemId": &types.AttributeValueMemberS{Value: foodItem.ItemId},
+			"name":   &types.AttributeValueMemberS{Value: foodItem.Name},
+			"time":   &types.AttributeValueMemberS{Value: "12345"},
+		},
 	})
+
+	if err != nil {
+		panic(err)
+	}
 
 	return events.APIGatewayProxyResponse{
 			StatusCode: 201,
-			Body:       strconv.FormatBool(result),
 		},
 		err
 }
